edc4494299c01873e543c18547dc40b1e78660c2
Victor Moreno Jiménez <victor.moreno@connectedhealthservices.com>
2021-09-19 12:57:17 +0200

    Create test

--- test/cdk-sample.test.ts
+++ test/cdk-sample.test.ts
@@ -1,13 +1,12 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { SynthUtils } from '@aws-cdk/assert'
+import '@aws-cdk/assert/jest'
 import * as cdk from '@aws-cdk/core';
 import * as CdkSample from '../lib/cdk-sample-stack';
 
-test('Empty Stack', () => {
+test('Test MyCdkSampleStack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new CdkSample.CdkSampleStack(app, 'MyTestStack');
+    const stack = new CdkSample.CdkSampleStack(app, 'MyCdkSampleStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot()
 });
